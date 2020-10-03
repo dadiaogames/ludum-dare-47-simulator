e27678e316196d536bc472571b480a11f7be98bf
jakehsiao <1499975101@qq.com>
2020-10-03 09:28:43 +0800

    Updates

---
+++ precache-manifest.0ecd84b64e463f47475e0a8e568fe8cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a608f2a7415bc5ad9bf5c82beb965ac",
+    "url": "/dadiaogames.github.io/ludum-dare-47-simulator/index.html"
+  },
+  {
+    "revision": "9fdf4792399bcd4e92d3",
+    "url": "/dadiaogames.github.io/ludum-dare-47-simulator/static/css/main.41756ca6.chunk.css"
+  },
+  {
+    "revision": "581c2f46f4d528d4224c",
+    "url": "/dadiaogames.github.io/ludum-dare-47-simulator/static/js/2.2439d46a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dadiaogames.github.io/ludum-dare-47-simulator/static/js/2.2439d46a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fdf4792399bcd4e92d3",
+    "url": "/dadiaogames.github.io/ludum-dare-47-simulator/static/js/main.654741d8.chunk.js"
+  },
+  {
+    "revision": "4121fcef92bc50eececf",
+    "url": "/dadiaogames.github.io/ludum-dare-47-simulator/static/js/runtime-main.24fedbd9.js"
+  }
+]);
